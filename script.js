e010f8f1d4009a96a675abf2aa3173a8519e8f2e
ManishaBalagoni <141826137+ManishaBalagoni@users.noreply.github.com>
2023-08-29 20:06:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const calculateTemp = () => { 
+    const inputTempElement = document.getElementById('temp').value;
+    const tempSelectedElement = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelectedElement.selectedIndex].value;
+
+    // converting celsius to fahrenheit
+
+    const celToFah = (cel) => {
+        let fahrenheit = ((cel * 9 / 5) + 32).toFixed(1);
+        return fahrenheit;
+    }
+
+    // converting fahrenheit to celsius
+
+    const fahToCal = (fah) => {
+        let celsius = ((fah - 32) * 5 / 9).toFixed(1);
+        return celsius;
+    }
+
+    if (valueTemp == 'cel') {
+        document.getElementById("result").innerHTML ="-->  " + celToFah(inputTempElement) + "&#176; Fahrenheit" + "  <--";
+    }
+
+    else{
+        document.getElementById("result").innerHTML = fahToCal(inputTempElement) + "&#176; Celsius";
+    }
+
+}
